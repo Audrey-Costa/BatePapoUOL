6641f9d0d2a22024d27dde82b66af61dc56cf1bd
Audrey-Costa <audreycosta@nano.ufrj.br>
2022-04-18 12:48:18 -0300

    Correção de bugs e melhorias de código

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 let messages = [];
+let userName = "Todos";
+let privacy = "message"
 
 function logIn(){
     let user = {
         name: `${document.querySelector("input").value}`
     }
-    console.log(user)
     document.querySelector("input").classList.add("hide")
     document.querySelector("button").classList.add("hide")
     document.querySelector(".load").classList.remove("hide")
@@ -15,11 +16,9 @@ function logIn(){
 
 function logon(){
     let intervalIDLogin = setInterval(loginMaintenance, 4000)
-    console.log(intervalIDLogin)
     let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     promise.then(renderMessages)
     let intervalIDLoadMessages = setInterval(callMessages, 3000)
-    console.log(intervalIDLoadMessages)
 }
 
 function loginMaintenance(){
@@ -39,25 +38,37 @@ function fail(error){
     if (error.response.status === 500){
         alert("Perda de comunicação com o servidor.")
     }
-
     window.location.reload()
+}
+
+function fail2(error){
+    if (error.response.status === 400){
+        return
+    }
 }
 
 function renderMessages(answer){
     document.querySelector("ul").innerHTML = ""
     messages = answer.data
-    console.log(messages)
     document.querySelector(".container-1").classList.add("hide")
     document.querySelector(".container-2").classList.remove("hide")
     for (let i = 0; i < messages.length; i++){
-        if ((messages[i].type) === "status"){
-            document.querySelector("ul").innerHTML += "<li><div class='messages status'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + `${messages[i].text}` + "</div></li>"
-        }
+//        if ((messages[i].type) === "status"){
+//            document.querySelector("ul").innerHTML += "<li><div class='messages status'>" + //`${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + `${messages[i].//text}` + "</div></li>"
+//        }
         if((messages[i].type) === "message"){
             if (messages[i].to === "Todos"){
-                document.querySelector("ul").innerHTML += "<li><div class='messages all'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + "para" + "&nbsp" + "<h1>Todos:</h1>" + "&nbsp" + `${messages[i].text}` + "</div></li>"
+                document.querySelector("ul").innerHTML += "<li><div class='messages all'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + "para" + "&nbsp" + `<h1>${messages[i].to}:</h1>` + "&nbsp" + `${messages[i].text}` + "</div></li>"
             }
             else{
+                document.querySelector("ul").innerHTML += "<li><div class='messages all'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + "para" + "&nbsp" + `<h1>${messages[i].to}:</h1>` + "&nbsp" + `${messages[i].text}` + "</div></li>"
+            }
+        }
+        if ((messages[i].type) === "private_message"){
+            if (messages[i].to === "Todos"){
+                document.querySelector("ul").innerHTML += "<li><div class='messages reserved'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + "para" + "&nbsp" + `<h1>${messages[i].to}:</h1>` + "&nbsp" + `${messages[i].text}` + "</div></li>"
+            }
+            if (messages[i].to === document.querySelector("input").value){
                 document.querySelector("ul").innerHTML += "<li><div class='messages reserved'>" + `${messages[i].time}` + "&nbsp" + `<h1>${messages[i].from}</h1>` + "&nbsp" + "reservadamente para" + "&nbsp" + `<h1>${messages[i].to}:</h1>` + "&nbsp" + `${messages[i].text}` + "</div></li>"
             }
         }
@@ -71,9 +82,6 @@ function callMessages(){
 }
 
 function compareMessages(answer){
-    //console.log(answer.data[99].time)
-    //console.log(messages[99].time)
-    //console.log(answer.data[99].time !== messages[99].time)
     if (answer.data[99].time !== messages[99].time){
         renderMessages(answer)
     }
@@ -82,15 +90,69 @@ function compareMessages(answer){
 function sendMessage(){
     let message = {
         from: `${document.querySelector("input").value}`,
-        to: "Todos",
+        to: `${userName}`,
         text: `${document.querySelector(".bottom input").value}`,
-        type: "message"
+        type: `${privacy}`
     }
+    console.log(message)
     let promisePost = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", message)
     let promiseGet
     promisePost.then(promiseGet = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages"))
     promisePost.then(document.querySelector(".bottom input").value = "")
-    promisePost.catch(fail)
-    promiseGet.then(console.log)
+    promisePost.catch(fail2)
     promiseGet.then(renderMessages)
+}
+
+function menuOpen(){
+    let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    document.querySelector(".menu").classList.toggle("open")
+    document.querySelector(".backgroundMenu").classList.toggle("hide")
+    userName = "todos"
+    promise.then(gerarUsersList)
+}
+
+function menuClose(){
+    document.querySelector(".menu").classList.toggle("open")
+    document.querySelector(".backgroundMenu").classList.toggle("hide")
+    console.log(privacy)
+}
+
+function gerarUsersList(answer){
+    let listMenu = document.querySelector(".pessoas ul")
+    let listUsers = answer.data
+    listMenu.innerHTML = `<li><div onclick="selectUser(this)"><ion-icon name="people"></ion-icon><p>Todos</p><ion-icon class="check" name="checkmark-outline" ></ion-icon></div></li>`
+    for (let i = 0; i < listUsers.length; i++){
+        if (listUsers[i].name !== document.querySelector("input").value){
+            listMenu.innerHTML += `<li><div onclick="selectUser(this)"><ion-icon name="people"></ion-icon><p>${listUsers[i].name}</p><ion-icon class="check hide" name="checkmark-outline" ></ion-icon></div></li>`
+        }
+    }
+}
+
+function selectUser(element){
+    let listUserSelectors = document.querySelectorAll(".pessoas .check")
+    for (let i = 0; i < listUserSelectors.length; i++){
+        listUserSelectors[i].classList.add("hide")
+    }
+    element.childNodes[2].classList.remove("hide")
+    userName = element.childNodes[1].innerHTML
+    return userName
+}
+
+function selectPrivacy(element){
+    let listPrivacy = document.querySelectorAll(".options .check")
+    listPrivacy[0].classList.add("hide")
+    listPrivacy[1].classList.add("hide")
+    element.childNodes[2].classList.remove("hide")
+    if (listPrivacy[0].classList.contains("hide")){
+        return privacy = "private_message"
+    } else{
+        return privacy = "message"
+    }
+}
+
+function logout(){
+    document.querySelector("input").value = ""
+    document.querySelector(".bottom input").value = ""
+    alert("Volte sempre!")
+    window.location.reload()
 }
